42b6244fd3afd432022c68ad368b95d513314a08
张以白 <zhangtongding@foxmail.com>
2023-04-19 01:42:31 +0800

    excel参数化之参数化运行,先编写了单个接口时的参数化,然后多个用例参数实现为:使用pytest进行参数化测试,run_list获取数据列表,data_key类属性获取数据,发送请求判断参数是否是否为空,并进行转义

--- common/ExcelConfig.py
+++ common/ExcelConfig.py
@@ -1,12 +1,12 @@
 class DataConfig:
     # 定义列属性
-    case_id="用例名称"
+    case_id="用例ID"
     case_model="模块"
     case_name="接口名称"
     url="请求URL"
     pre_exec="前置条件"
     method="请求类型"
-    params_type="参数类型"
+    params_type="请求参数类型"
     params="请求参数"
     expect_result="预期结果"
     actual_result="实际结果"
---
+++ testcase/test_excel_case.py
@@ -0,0 +1,122 @@
+import os
+import json
+
+import pytest
+
+from common.ExcelData import Data
+from config.Conf import ConfigYaml
+from utils.LogUtil import my_log
+from common import ExcelConfig
+from utils.RequesetsUtil import Request
+
+# 多个用例的
+# 1 初始化用例文件
+# ① 初始化用例文件
+case_file = os.path.join("../data", ConfigYaml().get_excel_file())
+# ② 获取用例sheet名称
+sheet_name = ConfigYaml().get_excel_sheet()
+# ③ 获取运行测试用例列表
+run_list = Data(case_file, sheet_name).get_run_data()
+# ④ 日志
+log = my_log()
+
+
+class TestExcel:
+    # ① 初始化信息
+    # 1增加pytest方法
+    @pytest.mark.parametrize("case", run_list)
+    # 2 修改方法參數
+    def test_run(self, case):
+        data_key = ExcelConfig.DataConfig
+        # 3 重构函数内容
+        url = ConfigYaml().get_conf_url() + case[data_key.url]
+        """在run_list[0][data_key.url]中，run_list[0]表示取出运行测试用例列表中的第一条测试用例，data_key是一个常量类，里面定义了Excel表格中各列的列名，data_key.url表示取出该测试用例中名为“url”的一列的值。所以run_list[0][data_key.url]的含义是取出运行测试用例列表中第一条测试用例中的“url”这一列的值。"""
+        # print(url)
+        case_id = case[data_key.case_id]
+        case_model = case[data_key.case_model]
+        case_name = case[data_key.case_name]
+        pre_exec = case[data_key.pre_exec]
+        method = case[data_key.method]
+        params = case[data_key.params]
+        params_type = case[data_key.params_type]
+        expect_result = case[data_key.expect_result]
+        headers = case[data_key.headers]
+        cookies = case[data_key.cookies]
+        code = case[data_key.code]
+        db_verify = case[data_key.db_verify]
+        # print(case_model,case_name,pre_exec,method,params,params_type,expect_result,headers,cookies,code,db_verify)
+
+        # ② 发送请求
+        request = Request()
+        # 验证params有没有内容
+        if len(str(params).strip()) is not 0:
+            # params转义成json
+            params = json.loads(params)
+        # get/post
+        if str(method).lower() == "get":
+            res = request.get(url, json=params)
+        elif str(method).lower() == "post":
+            res = request.post(url, json=params)
+        else:
+            log.error("这是一个错误请求:%s" % method)
+        print(res)
+
+
+# 2 测试用例方法,参数化运行
+
+if __name__ == '__main__':
+    # TestExcel.test_run()
+    # 4 运行
+    pytest.main(["-s", "test_excel_case.py"])
+
+"""一个用例
+# 1 初始化用例文件
+# ① 初始化用例文件
+case_file = os.path.join("../data", ConfigYaml().get_excel_file())
+# ② 获取用例sheet名称
+sheet_name = ConfigYaml().get_excel_sheet()
+# ③ 获取运行测试用例列表
+run_list = Data(case_file, sheet_name).get_run_data()
+# ④ 日志
+log = my_log()
+
+class TestExcel:
+    # ① 初始化信息
+    def test_run(self):
+        data_key = ExcelConfig.DataConfig
+        url = ConfigYaml().get_conf_url() + run_list[0][data_key.url]
+        在run_list[0][data_key.url]中，run_list[0]表示取出运行测试用例列表中的第一条测试用例，data_key是一个常量类，里面定义了Excel表格中各列的列名，data_key.url表示取出该测试用例中名为“url”的一列的值。所以run_list[0][data_key.url]的含义是取出运行测试用例列表中第一条测试用例中的“url”这一列的值。
+        print(url)
+        case_id = run_list[0][data_key.case_id]
+        case_model = run_list[0][data_key.case_model]
+        case_name = run_list[0][data_key.case_name]
+        pre_exec = run_list[0][data_key.pre_exec]
+        method = run_list[0][data_key.method]
+        params = run_list[0][data_key.params]
+        params_type = run_list[0][data_key.params_type]
+        expect_result = run_list[0][data_key.expect_result]
+        headers = run_list[0][data_key.headers]
+        cookies = run_list[0][data_key.cookies]
+        code = run_list[0][data_key.code]
+        db_verify = run_list[0][data_key.db_verify]
+        #print(case_model,case_name,pre_exec,method,params,params_type,expect_result,headers,cookies,code,db_verify)
+
+        # ② 发送请求
+        request = Request()
+        # 验证params有没有内容
+        if len(str(params).strip()) is not 0:
+            # params转义成json
+            params = json.loads(params)
+        # get/post
+        if str(method).lower() == "get":
+            res = request.get(url, json=params)
+        elif str(method).lower() == "post":
+            res = request.post(url, json=params)
+        else:
+            log.error("这是一个错误请求:%s"%method)
+        print(res)
+# 2 测试用例方法,参数化运行
+
+if __name__ == '__main__':
+    TestExcel.test_run()
+"""
